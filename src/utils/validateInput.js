29c5bbf6ccd9f589d53ff477f0358d00353b6ae7
7777 <vermas8900@gmail.com>
2023-07-01 15:03:51 +0700

    Fixing the validateInput

--- src/utils/validateInput.js
+++ src/utils/validateInput.js
@@ -1,39 +1,42 @@
 import { Alert } from "react-native";
 
-export const validateInputs = (name, email, password, confirmPassword) => {
-  if (!name || !email || !password || !confirmPassword) {
+export const validateInputs = (username ,name, email, password, confirmPassword) => {
+  if (!username || !name || !email || !password || !confirmPassword) {
     Alert.alert('Vui lòng điền đầy đủ thông tin.');
-    return;
+    return false;
   }
 
   if (password !== confirmPassword) {
     Alert.alert('Mật khẩu và xác nhận lại mật khẩu không khớp.');
-    return;
+    return false;
   }
-  if (name === null || name.trim() === '') {
-    // Kiểm tra nếu tên đăng nhập rỗng
+
+  if (!name || name.trim() === '') {
+    Alert.alert('Vui lòng nhập tên của bạn');
+    return false;
+  }
+
+  if (!username || username.trim() === '') {
     Alert.alert('Vui lòng nhập tên đăng nhập');
     return false;
   }
 
-  if (email === null || email.trim() === '') {
-    // Kiểm tra nếu email rỗng
+  if (!email || email.trim() === '') {
     Alert.alert('Vui lòng nhập email');
     return false;
   }
 
-  // Kiểm tra định dạng email hợp lệ
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email.trim())) {
     Alert.alert('Vui lòng nhập email hợp lệ');
     return false;
   }
 
-  if (password === null || password.trim() === '') {
-    // Kiểm tra nếu mật khẩu rỗng
+  if (!password || password.trim() === '') {
     Alert.alert('Vui lòng nhập mật khẩu');
     return false;
   }
-
+  // console.log(username);
   return true;
 };
+
